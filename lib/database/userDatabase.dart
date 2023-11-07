59a0f00fc7d47bf9d89ec92543b861041f2a1352
benthen <nanhui0508@gmail.com>
2023-11-08 01:08:35 +0800

    last update

--- lib/database/userDatabase.dart
+++ lib/database/userDatabase.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 
 class UserDatabase {
   Future userRegister(mykad, name, phone, email, race, password) async {
